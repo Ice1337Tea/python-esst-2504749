f92a61e2120f5b4dee86d9640466494d179913ef
Ice1337Tea <rafael-oliwa@t-online.de>
2025-03-30 15:14:52 +0000

    Rechtschreibfehler

--- uebungs_datei.py
+++ uebungs_datei.py
@@ -1,3 +1,9 @@
 # Das ist ein Kommentar in Ihrer Uebungsdatei
 
 print('Hallo Welt!')
+
+meine_erste_variable = 4
+meine_zweite_variable = 2.3
+mein_ergebnis = meine_erste_variable + meine_zweite_variable
+print (";eom Ergnmos", mein_ergebnis)
+
