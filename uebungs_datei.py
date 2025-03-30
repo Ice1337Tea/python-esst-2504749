aca06fbff03db36c57f50d5176bdf95ee559f4eb
Ice1337Tea <rafael-oliwa@t-online.de>
2025-03-30 15:15:22 +0000

    Einfach machen

--- uebungs_datei.py
+++ uebungs_datei.py
@@ -5,5 +5,5 @@ print('Hallo Welt!')
 meine_erste_variable = 4
 meine_zweite_variable = 2.3
 mein_ergebnis = meine_erste_variable + meine_zweite_variable
-print (";eom Ergnmos", mein_ergebnis)
+print ("Mein Ergebnis", mein_ergebnis)
 
